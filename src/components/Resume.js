ad5506c8635cb8b062c3f8a44e6df258b8c9b994
smakena74 <smakena74@gmail.com>
2026-02-03 08:54:27 +0000

    uploaded resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <div className="resume">
       <h2>Resume</h2>
-      <a href="/resume.pdf" download className="download-btn">
+      <a href="/sello_makena.pdf" download className="download-btn">
         Download my resume (PDF)
       </a>
     </div>
